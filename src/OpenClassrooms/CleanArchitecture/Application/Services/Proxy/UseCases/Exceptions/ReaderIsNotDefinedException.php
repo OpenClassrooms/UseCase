94576b3f6563bd4a17b249b6b54be63d6f3493b8
Romain Kuzniak <romain.kuzniak@simple-it.fr>
2014-05-09 19:03:49 +0200

    add improvement on proxy builder

---
+++ src/OpenClassrooms/CleanArchitecture/Application/Services/Proxy/UseCases/Exceptions/ReaderIsNotDefinedException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OpenClassrooms\CleanArchitecture\Application\Services\Proxy\UseCases\Exceptions;
+
+/**
+ * @author Romain Kuzniak <romain.kuzniak@openclassrooms.com>
+ */
+class ReaderIsNotDefinedException extends \Exception
+{
+    
+}
--- tests/OpenClassrooms/CleanArchitecture/Application/Services/Proxy/UseCases/UseCaseProxyBuilderTest.php
+++ tests/OpenClassrooms/CleanArchitecture/Application/Services/Proxy/UseCases/UseCaseProxyBuilderTest.php
@@ -2,10 +2,19 @@
 
 namespace OpenClassrooms\Tests\CleanArchitecture\Application\Services\Proxy\UseCases;
 
+use Doctrine\Common\Annotations\AnnotationReader;
 use
     OpenClassrooms\CleanArchitecture\Application\Services\Proxy\UseCases\Impl\UseCaseProxyBuilderImpl;
 use OpenClassrooms\CleanArchitecture\Application\Services\Proxy\UseCases\UseCaseProxyBuilder;
-use OpenClassrooms\Tests\CleanArchitecture\BusinessRules\UseCases\UseCaseStub;
+use OpenClassrooms\Tests\CleanArchitecture\Application\Services\Cache\CacheSpy;
+use OpenClassrooms\Tests\CleanArchitecture\Application\Services\Event\EventFactorySpy;
+use OpenClassrooms\Tests\CleanArchitecture\Application\Services\Event\EventSpy;
+use OpenClassrooms\Tests\CleanArchitecture\Application\Services\Security\SecuritySpy;
+use OpenClassrooms\Tests\CleanArchitecture\Application\Services\Transaction\TransactionSpy;
+use OpenClassrooms\Tests\CleanArchitecture\BusinessRules\Requestors\UseCaseRequestStub;
+use OpenClassrooms\Tests\CleanArchitecture\BusinessRules\Responders\UseCaseResponseStub;
+use
+    OpenClassrooms\Tests\CleanArchitecture\BusinessRules\UseCases\Workflow\AllAnnotationsUseCaseStub;
 
 /**
  * @author Romain Kuzniak <romain.kuzniak@openclassrooms.com>
@@ -30,18 +39,31 @@ class UseCaseProxyBuilderTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
+     * @expectedException \OpenClassrooms\CleanArchitecture\Application\Services\Proxy\UseCases\Exceptions\ReaderIsNotDefinedException
+     */
+    public function WithoutReader_Build_ThrowException()
+    {
+        $this->builder->forUseCase(new AllAnnotationsUseCaseStub())->build();
+    }
+
+    /**
+     * @test
      */
     public function Build_ReturnUseCaseProxy()
     {
         $proxy = $this->builder
-            ->forUseCase(new UseCaseStub())
+            ->forUseCase(new AllAnnotationsUseCaseStub())
+            ->withReader(new AnnotationReader())
+            ->withCache(new CacheSpy())
+            ->withEvent(new EventSpy())
+            ->withEventFactory(new EventFactorySpy())
+            ->withSecurity(new SecuritySpy())
+            ->withTransaction(new TransactionSpy())
             ->build();
 
-        $this->assertInstanceOf(
-            self::USE_CASE_PROXY_CLASS,
-            $proxy
-        );
-        $this->assertEquals(new UseCaseStub(), $proxy->getUseCase());
+        $this->assertInstanceOf(self::USE_CASE_PROXY_CLASS, $proxy);
+        $this->assertEquals(new AllAnnotationsUseCaseStub(), $proxy->getUseCase());
+        $this->assertEquals(new UseCaseResponseStub(), $proxy->execute(new UseCaseRequestStub()));
     }
 
     protected function setUp()
