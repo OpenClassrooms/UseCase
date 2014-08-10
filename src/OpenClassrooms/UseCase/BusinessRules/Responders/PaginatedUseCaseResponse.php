a22659664c3c50f2f85ec27a30eb02d5f52f15d8
Romain Kuzniak <romain.kuzniak@simple-it.fr>
2014-08-10 20:45:35 +0200

    improve PaginatedResponse

--- src/OpenClassrooms/UseCase/BusinessRules/Responders/PaginatedUseCaseResponse.php
+++ src/OpenClassrooms/UseCase/BusinessRules/Responders/PaginatedUseCaseResponse.php
@@ -5,26 +5,121 @@ namespace OpenClassrooms\UseCase\BusinessRules\Responders;
 /**
  * @author Kévin Letord <kevin.letord@simple-it.fr>
  */
-interface PaginatedUseCaseResponse extends UseCaseResponse
+abstract class PaginatedUseCaseResponse implements UseCaseResponse
 {
+    /**
+     * @var array
+     */
+    protected $items;
+
+    /**
+     * @var integer
+     */
+    protected $totalItems;
+
+    /**
+     * @var integer
+     */
+    protected $itemsPerPage;
+
+    /**
+     * @var integer
+     */
+    protected $currentPage = 1;
+
+    /**
+     * @var integer
+     */
+    protected $totalPages = 1;
+
+    /**
+     * @var integer
+     */
+    protected $firstItemIndex;
+
+    /**
+     * @var integer
+     */
+    protected $lastItemIndex;
+
+    /**
+     * @return int
+     */
+    public function getCurrentPage()
+    {
+        return $this->currentPage;
+    }
+
+    public function setFirstItemIndex($firstItemIndex)
+    {
+        $this->firstItemIndex = $firstItemIndex;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFirstItemIndex()
+    {
+        return $this->firstItemIndex;
+    }
+
+    public function setItems(array $items)
+    {
+        $this->items = $items;
+    }
+
     /**
      * @return array
      */
-    public function getItems();
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    public function setItemsPerPage($itemsPerPage)
+    {
+        $this->itemsPerPage = $itemsPerPage;
+    }
 
     /**
      * @return int
      */
-    public function getTotalItems();
+    public function getItemsPerPage()
+    {
+        return $this->itemsPerPage;
+    }
+
+    public function setLastItemIndex($lastItemIndex)
+    {
+        $this->lastItemIndex = $lastItemIndex;
+    }
 
     /**
      * @return int
      */
-    public function getFirstItemIndex();
+    public function getLastItemIndex()
+    {
+        return $this->lastItemIndex;
+    }
+
+    public function setTotalItems($totalItems)
+    {
+        $this->totalItems = $totalItems;
+    }
 
     /**
      * @return int
      */
-    public function getLastItemIndex();
+    public function getTotalItems()
+    {
+        return $this->totalItems;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalPages()
+    {
+        return $this->totalPages;
+    }
 }
- 
